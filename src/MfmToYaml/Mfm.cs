ec90d25f99385090321332d8c7835acd4addb035
ArchLeaders <archleadership28@gmail.com>
2024-04-22 15:17:57 -0700

    Impl program

---
+++ src/MfmToYaml/Mfm.cs
@@ -0,0 +1,89 @@
+﻿using MfmToYaml.Models;
+using MfmToYaml.Readers;
+using System.Buffers;
+using VYaml.Emitter;
+
+namespace MfmToYaml;
+
+public class Mfm
+{
+    public Dictionary<string, FunctionEnum> Enums { get; } = [];
+    public Dictionary<(int, int), Function> Functions { get; } = [];
+
+    public Mfm(Span<byte> data)
+    {
+        MfmReader reader = new(data, this);
+        reader.Read();
+
+        foreach (var (_, function) in Functions) {
+            FormatName(function.Name);
+            foreach (var (type, name) in function.Parameters) {
+                FormatName(name);
+
+                if (Enums.ContainsKey(type)) {
+                    FormatName(type);
+                }
+            }
+        }
+
+        foreach (var (name, functionEnum)  in Enums) {
+            FormatName(name);
+            foreach (var (_, enumValueName) in functionEnum) {
+                FormatName(enumValueName);
+            }
+        }
+    }
+
+    public void Write(IBufferWriter<byte> writer)
+    {
+        Utf8YamlEmitter emitter = new(writer);
+        emitter.BeginMapping();
+        {
+            foreach (var (key, functionEnum) in Enums) {
+                emitter.WriteString(key);
+                emitter.BeginMapping();
+                {
+                    foreach (var (enumKey, enumName) in functionEnum) {
+                        emitter.WriteInt64(enumKey);
+                        emitter.WriteString(enumName);
+                    }
+                }
+                emitter.EndMapping();
+            }
+        }
+        emitter.EndMapping();
+
+        emitter.WriteRaw("---"u8, indent: false, lineBreak: true);
+
+        emitter.BeginMapping();
+        {
+            foreach (var ((group, type), function) in Functions) {
+                emitter.BeginSequence(SequenceStyle.Flow);
+                {
+                    emitter.WriteInt32(group);
+                    emitter.WriteInt32(type);
+                }
+                emitter.EndSequence();
+
+                emitter.Tag($"!{function.Name}");
+                emitter.BeginSequence();
+                {
+                    foreach (var (paramType, paramName) in function.Parameters) {
+                        emitter.Tag($"!{paramType}");
+                        emitter.WriteString(paramName);
+                    }
+                }
+                emitter.EndSequence();
+            }
+        }
+        emitter.EndMapping();
+    }
+
+    private unsafe void FormatName(string name)
+    {
+        fixed (char* ptr = name) {
+            Span<char> chars = new(ptr, name.Length);
+            chars[0] = char.ToUpper(chars[0]);
+        }
+    }
+}
--- src/MfmToYaml/Program.cs
+++ src/MfmToYaml/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommunityToolkit.HighPerformance.Buffers;
+using MfmToYaml;
+using MfmToYaml.Extensions;
+using System.Buffers;
+using System.Reflection;
+
+Console.WriteLine($"""
+    MSBT Function Map (MFM) to YAML [Version {Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "???"}]
+    (c) 2024 ArchLeaders. MIT.
+
+    """);
+
+if (args.Length == 0 || args[0].ToLower() is "h" or "help" or "-h" or "--help") {
+    Console.WriteLine("""
+          Usage:
+        <input> [-o|--output OUTPUT] ...
+
+          Documentation:
+        github.com/ArchLeaders/MfmToYaml
+        """);
+}
+
+for (int i = 0; i < args.Length; i++) {
+    string input = args[i];
+
+    using FileStream fs = File.OpenRead(input);
+    int size = Convert.ToInt32(fs.Length);
+    using SpanOwner<byte> buffer = SpanOwner<byte>.Allocate(size);
+    fs.Read(buffer.Span);
+    fs.Dispose();
+
+    if (!args.TryGetOutput(ref i, out string? output)) {
+        output = Path.ChangeExtension(input, "yaml");
+    }
+
+    Mfm mfm = new(buffer.Span);
+    ArrayBufferWriter<byte> writer = new();
+    mfm.Write(writer);
+
+    using FileStream outputFs = File.Create(output);
+    outputFs.Write(writer.WrittenSpan);
+}
